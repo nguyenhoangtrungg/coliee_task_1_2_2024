cc1e68eabaf24f08afe3529c4a19e0f8f58d8e5f
nguyenhoangtrungg <200200083@vnu.edu.vn>
2024-01-19 18:29:31 +0700

    update

--- core/infer_ense.py
+++ core/infer_ense.py
@@ -24,14 +24,14 @@ dev_df = run_create_csv_bm25(TRAINING_PATH, LABEL_PATH, CSV_TRAINING_DATA_PATH, 
 
 test_df = run_create_csv_bm25(TESTING_PATH, LABEL_PATH, CSV_TESTING_DATA_PATH, "infer", NEGATIVE_MODE, NEGATIVE_NUM)
 
-dev_dataset = model_dataset.MultilingualBertDataset(dev_df["fragment"].tolist(), dev_df["content"].tolist(), dev_df["label"].tolist())
-test_dataset = model_dataset.MultilingualBertDataset(test_df["fragment"].tolist(), test_df["content"].tolist(), test_df["label"].tolist())
+# dev_dataset = model_dataset.MultilingualBertDataset(dev_df["fragment"].tolist(), dev_df["content"].tolist(), dev_df["label"].tolist())
+# test_dataset = model_dataset.MultilingualBertDataset(test_df["fragment"].tolist(), test_df["content"].tolist(), test_df["label"].tolist())
 
 # t5_dev_dataset = t5_model_dataset.T5Dataset(dev_df["fragment"].tolist(), dev_df["content"].tolist(), dev_df["label"].tolist())
 # t5_test_dataset = t5_model_dataset.T5Dataset(test_df["fragment"].tolist(), test_df["content"].tolist(), test_df["label"].tolist())
 
 checkpoint_path = CHECKPOINT
-d_df = infer_model.encode_csv(dev_dataset, 1, checkpoint_path)
-t_df = infer_model.encode_csv(test_dataset, 1, checkpoint_path)
+d_df = infer_model.encode_csv(dev_df, 1, checkpoint_path)
+t_df = infer_model.encode_csv(test_df, 1, checkpoint_path)
 support_func.write_csv("/kaggle/working/" + "dev.csv", d_df)
 support_func.write_csv("/kaggle/working/" + "test.csv", t_df)
