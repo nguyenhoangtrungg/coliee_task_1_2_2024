a71f66cd8134bf36ca6f924c1add304e7c624402
phong Mr Truong Thang <test01@bright92.cm.cluster>
2025-03-11 15:32:51 +0700

    update

--- core/trainning.py
+++ core/trainning.py
@@ -14,6 +14,11 @@ from transformers import BertTokenizer, BertForSequenceClassification, TrainingA
 
 from constant import *
 
+import wandb
+os.environ["WANDB_API_KEY"] = 'd161976e53bc2384922e3ed5b3f8676c7b8a648d'
+
+wandb.login()
+
 def sigmoid(x):
   return 1 / (1 + math.exp(-x))
 
@@ -22,12 +27,12 @@ softmax_model = nn.Softmax(dim=1)
 
 # DATA LOADING
 if CHOOSE_WEAK == 0:
-    train_df = run_create_csv_bm25(TRAINING_PATH, LABEL_PATH, CSV_TRAINING_DATA_PATH, "train", NEGATIVE_MODE, NEGATIVE_NUM)
+    train_df = run_create_csv_bm25(TRAINING_PATH, TRAIN_LABEL_PATH, CSV_TRAINING_DATA_PATH, "train", NEGATIVE_MODE, NEGATIVE_NUM)
 
 elif CHOOSE_WEAK == 1:
     train_df = create_weak_dataset.create_weak_dataset(WEAK_DATASET_PATH, MIN_LEN, MAX_LEN)
 
-valid_df = run_create_csv_bm25(TESTING_PATH, LABEL_PATH, CSV_TESTING_DATA_PATH, "test", NEGATIVE_MODE, NEGATIVE_NUM)
+valid_df = run_create_csv_bm25(TESTING_PATH, TEST_LABEL_PATH, CSV_TESTING_DATA_PATH, "test", NEGATIVE_MODE, NEGATIVE_NUM)
 
 if FAST_DEV_RUN == "1":
     train_df = train_df[:40]
@@ -51,23 +56,28 @@ if FREEZE_MODE == 1:
         param.requires_grad = True
 
 training_args = TrainingArguments(
-    output_dir = '/kaggle/working/',
+    output_dir=OUTPUT_DIR,
+    do_train=True,
     learning_rate = LEARNING_RATE,
     num_train_epochs = N_EPOCH,
     per_device_train_batch_size = BATCH_SIZE,  
-    per_device_eval_batch_size= 1,
-    evaluation_strategy = "epoch",
-    logging_strategy="epoch",
+    per_device_eval_batch_size= BATCH_SIZE,
+    evaluation_strategy="epoch",
+    # logging_strategy="epoch",
     save_strategy="epoch",
+    report_to="wandb",
+    logging_steps=100,
+    # evaluation_strategy="steps",
+    logging_strategy="steps",
     disable_tqdm = False, 
-    load_best_model_at_end=True,
+    load_best_model_at_end=False,
     save_total_limit = 5
 )
 
 class CustomTrainer(Trainer):
     def compute_loss(self, model, inputs, return_outputs=False):
         label_rate = [1.0, float(W_LOSS)]
-        print("Rate:", label_rate)
+        # print("Rate:", label_rate)
         labels = inputs.pop("labels")
         outputs = model(**inputs)
         logits = softmax_model(outputs.get("logits"))
@@ -98,10 +108,10 @@ else:
 print("Start training...")
 trainer.train()
 
-list_output_dir = os.listdir("/kaggle/working")
-for file in list_output_dir:
-    if "checkpoint" in file and "csv" not in file:
-        print("Found checkpoint:", file)
-        checkpoint_path = os.path.join("/kaggle/working", file)
-        df = infer_model.encode_csv(valid_df, 1, checkpoint_path)
-        support_func.write_csv(checkpoint_path + ".csv", df)
+# list_output_dir = os.listdir("/kaggle/working")
+# for file in list_output_dir:
+#     if "checkpoint" in file and "csv" not in file:
+#         print("Found checkpoint:", file)
+#         checkpoint_path = os.path.join("/kaggle/working", file)
+#         df = infer_model.encode_csv(valid_df, 1, checkpoint_path)
+#         support_func.write_csv(checkpoint_path + ".csv", df)
